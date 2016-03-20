ccc540e3e966251cf053f51107399ed5d32bdce9
senakamura <seanenakamura@gmail.com>
2016-03-19 17:09:23 -1000

    dries up index

--- index.js
+++ index.js
@@ -14,77 +14,20 @@ async.series([
 
     modules.randomizeQuarters();
 
-    prompt.get(['leftScale', 'rightScale'], function(err, res){
-      if (err){return onErr(err)}
-      console.log('you have two more tries');
-      console.log('[ ' + res.leftScale + ', ' + res.rightScale + ' ]');
-      var uno = new Array();
-      var dos = new Array();
-      uno = res.leftScale.split('').map(Number);
-      dos = res.rightScale.split('').map(Number);
-
-      if(res.leftScale.length === 1 && res.rightScale.length === 1 && res.leftScale[0] === res.rightScale[0]){
-        var value = modules.quarterScale(uno, dos);
-        if (value[0] === true && value[1] === true){
-          process.stdout.write('\n Congratulations! You did it!');
-          process.exit();
-        }else{
-          callback(console.log(modules.quarterScale(uno,dos)));
-        }
-      }else {
-        callback(console.log(modules.quarterScale(uno, dos)));
-      }
-    });
+    return promptScale(callback);
 
   },
 
   function (callback){
     prompt.start();
-    prompt.get(['leftScale', 'rightScale'], function(err, res){
-      if (err){return onErr(err)}
-      console.log('you have one more tries');
-      console.log('[ ' + res.leftScale + ', ' + res.rightScale + ' ]');
-      var uno = new Array();
-      var dos = new Array();
-      uno = res.leftScale.split('').map(Number);
-      dos = res.rightScale.split('').map(Number);
 
-      if(res.leftScale.length === 1 && res.rightScale.length === 1 && res.leftScale[0] === res.rightScale[0]){
-        var value = modules.quarterScale(uno, dos);
-        if (value[0] === true && value[1] === true){
-          process.stdout.write('\n Congratulations! You did it!');
-          process.exit();
-        }else{
-          callback(console.log(modules.quarterScale(uno,dos)));
-        }
-      }else {
-        callback(console.log(modules.quarterScale(uno, dos)));
-      }
-    });
+    return promptScale(callback);
   },
 
   function (callback){
     prompt.start();
-    prompt.get(['leftScale', 'rightScale'], function(err, res){
-      if (err){return onErr(err)}
-      console.log('[ ' + res.leftScale + ', ' + res.rightScale + ' ]');
-      var uno = new Array();
-      var dos = new Array();
-      uno = res.leftScale.split('').map(Number);
-      dos = res.rightScale.split('').map(Number);
 
-      if(res.leftScale.length === 1 && res.rightScale.length === 1 && res.leftScale[0] === res.rightScale[0]){
-        var value = modules.quarterScale(uno, dos);
-        if (value[0] === true && value[1] === true){
-          process.stdout.write('\n Congratulations! You did it!');
-          process.exit();
-        }else{
-          callback(console.log(modules.quarterScale(uno,dos)));
-        }
-      }else {
-        callback(console.log(modules.quarterScale(uno, dos)));
-      }
-    });
+    return promptScale(callback);
   }
 
   ],
@@ -94,10 +37,32 @@ async.series([
   });
 }
 
-
 function onErr(err){
   console.log(err);
   return 1;
 }
+
+var promptScale = function (callback){
+  prompt.get(['leftScale', 'rightScale'], function(err, res){
+    if (err){return onErr(err)}
+    console.log('[ ' + res.leftScale + ', ' + res.rightScale + ' ]');
+    var uno = new Array();
+    var dos = new Array();
+    uno = res.leftScale.split('').map(Number);
+    dos = res.rightScale.split('').map(Number);
+
+    if(res.leftScale.length === 1 && res.rightScale.length === 1 && res.leftScale[0] === res.rightScale[0]){
+      var value = modules.quarterScale(uno, dos);
+      if (value[0] === true && value[1] === true){
+        process.stdout.write('\n Congratulations! You did it!');
+        process.exit();
+      }else{
+        callback(console.log(modules.quarterScale(uno,dos)));
+      }
+    }else {
+      callback(console.log(modules.quarterScale(uno, dos)));
+    }
+  });
+};
 
 game();
